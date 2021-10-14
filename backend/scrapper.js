92a54c186ec74bffa0cada9c1bfae817ac53cd45
Kasper Seweryn <github@wvffle.net>
2021-10-14 09:53:26 +0200

    feat: add caching

--- backend/scrapper.js
+++ backend/scrapper.js
@@ -1,10 +1,20 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
+import NodeCache from 'node-cache'
 
 const TEACHER_LIST_URL = 'https://degra.wi.pb.edu.pl/rozklady/rozklad.php?page=nau'
 const TEACHER_URL = `${TEACHER_LIST_URL}&id=`
 
-const fetchTeacherList = async () => {
+const KEY_TEACHERS = 'teachers'
+
+const cache = new NodeCache()
+
+export const fetchTeacherList = async () => {
+  const cached = cache.get(KEY_TEACHERS)
+  if (cached) {
+    return cached
+  }
+
   const { data } = await axios.get(TEACHER_LIST_URL)
   const $ = cheerio.load(data)
 
@@ -14,7 +24,7 @@ const fetchTeacherList = async () => {
     res[+el.val()] = el.text()
   })
 
+  cache.set(KEY_TEACHERS, res, 1e5 * 36 * 24)
   return res
 }
 
-fetchTeacherList().then(console.log)
